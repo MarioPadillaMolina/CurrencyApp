e1cd93a7fdda59e682e2e72e225c102a2bba8141
root <root@cloud9-60.ieszaidinvergeles.org>
2020-11-15 19:17:32 +0000

    currency icon changed

--- resources/views/backend/base.blade.php
+++ resources/views/backend/base.blade.php
@@ -178,7 +178,7 @@
                with font-awesome or any other icon font library -->
                         <li class="nav-item has-treeview {{ $currencyOpen ?? ''}}">
                             <a href="#" class="nav-link active">
-                                <i class="nav-icon fas fa-building"></i>
+                                <i class="nav-icon fas fa-money-bill"></i>
                                 <p>
                                     Currencies
                                     <i class="right fas fa-angle-left"></i>
